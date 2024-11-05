b255086e78e824e1afecbfe8d55ef698c398cdde
Viveck Kingsley <166235217+viv3ckj@users.noreply.github.com>
2024-11-05 19:53:51 +0000

    Viv3ckj/testing definition (#45)
    Fix conditions of denominator exclusions and add test cases

--- analysis/pf_dataset.py
+++ analysis/pf_dataset.py
@@ -1,8 +1,7 @@
-from ehrql.tables.tpp import patients, case, when, clinical_events
+from ehrql.tables.tpp import case, when
 
 from pf_variables_library import check_pregnancy_status, count_past_events
 
-# Create dictionary of pharmacy first codes
 pharmacy_first_event_codes = {
     # # Community Pharmacy (CP) Blood Pressure (BP) Check Service (procedure)
     # "blood_pressure_service": ["1659111000000107"],
@@ -15,15 +14,6 @@ pharmacy_first_event_codes = {
     "combined_pf_service": ["1577041000000109", "983341000000102"],
 }
 
-def get_pf_consultation_ids_from_events(events,codelist):
-    pharmacy_first_ids = events.where(
-    events.snomedct_code.is_in(codelist)
-    ).consultation_id
-    return pharmacy_first_ids
-
-def get_pf_clinical_events(pharmacy_first_ids):
-    selected_events = clinical_events.where(clinical_events.consultation_id.is_in(pharmacy_first_ids))
-    return selected_events
 
 # Create denominator variables for each clinical condition
 # These are based on NHS England rules using sex, age, pregnancy status and repeated diagnoses
@@ -31,7 +21,9 @@ def get_pf_clinical_events(pharmacy_first_ids):
 # - urinary catheter for URT,
 # - bullous impetigo,
 # - chronic sinusitis and immunosuppressed individuals for acute sinusitis
-def get_uncomplicated_uti_denominator(index_date, selected_events, pregnancy_codelist):
+def get_uncomplicated_uti_denominator(
+    index_date, patients, selected_events, pregnancy_codelist
+):
     urt_code = ["1090711000000102"]
     count_urt_6m = count_past_events(index_date, selected_events, urt_code, 6)
     count_urt_12m = count_past_events(index_date, selected_events, urt_code, 12)
@@ -41,25 +33,25 @@ def get_uncomplicated_uti_denominator(index_date, selected_events, pregnancy_cod
         index_date, selected_events, pregnancy_codelist
     )
 
-    return (
-        (age >= 16)
-        & (age <= 64)
-        & (patients.sex.is_in(["female"]) & pregnancy_status.is_null())
-        | (count_urt_6m < 2)
-        | (count_urt_12m < 3)
-    )
+    inclusion_criteria = (age >= 16) & (age <= 64) & (patients.sex.is_in(["female"]))
+    exclusion_criteria = pregnancy_status | (count_urt_6m >= 2) | (count_urt_12m >= 3)
+
+    return inclusion_criteria & ~exclusion_criteria
 
 
-def get_shingles_denominator(index_date, selected_events, pregnancy_codelist):
+def get_shingles_denominator(index_date, patients, selected_events, pregnancy_codelist):
     age = patients.age_on(index_date)
     pregnancy_status = check_pregnancy_status(
         index_date, selected_events, pregnancy_codelist
     )
 
-    return (age >= 18) & pregnancy_status.is_null()
+    inclusion_criteria = age >= 18
+    exclusion_criteria = pregnancy_status
+
+    return inclusion_criteria & ~exclusion_criteria
 
 
-def get_impetigo_denominator(index_date, selected_events, pregnancy_codelist):
+def get_impetigo_denominator(index_date, patients, selected_events, pregnancy_codelist):
     impetigo_code = ["48277006"]
     count_impetigo_12m = count_past_events(
         index_date, selected_events, impetigo_code, 12
@@ -70,43 +62,57 @@ def get_impetigo_denominator(index_date, selected_events, pregnancy_codelist):
         index_date, selected_events, pregnancy_codelist
     )
 
-    return (
-        (age >= 1)
-        | (pregnancy_status.is_not_null() & (age >= 16))
-        | (count_impetigo_12m < 2)
-    )
+    inclusion_criteria = age >= 1
+    exclusion_criteria = (count_impetigo_12m >= 2) | (pregnancy_status & (age < 16))
+
+    return inclusion_criteria & ~exclusion_criteria
 
 
 def get_infected_insect_bites_denominator(
-    index_date, selected_events, pregnancy_codelist
+    index_date, patients, selected_events, pregnancy_codelist
 ):
     age = patients.age_on(index_date)
     pregnancy_status = check_pregnancy_status(
         index_date, selected_events, pregnancy_codelist
     )
 
-    return (age >= 1) | (pregnancy_status.is_not_null() & (age >= 16))
+    inclusion_criteria = age >= 1
+    exclusion_criteria = pregnancy_status & (age < 16)
+
+    return inclusion_criteria & ~exclusion_criteria
 
 
-def get_acute_sore_throat_denominator(index_date, selected_events, pregnancy_codelist):
+def get_acute_sore_throat_denominator(
+    index_date, patients, selected_events, pregnancy_codelist
+):
     age = patients.age_on(index_date)
     pregnancy_status = check_pregnancy_status(
         index_date, selected_events, pregnancy_codelist
     )
 
-    return (age >= 5) | (pregnancy_status.is_not_null() & (age >= 16))
+    inclusion_criteria = age >= 5
+    exclusion_criteria = pregnancy_status & (age < 16)
+
+    return inclusion_criteria & ~exclusion_criteria
 
 
-def get_acute_sinusitis_denominator(index_date, selected_events, pregnancy_codelist):
+def get_acute_sinusitis_denominator(
+    index_date, patients, selected_events, pregnancy_codelist
+):
     age = patients.age_on(index_date)
     pregnancy_status = check_pregnancy_status(
         index_date, selected_events, pregnancy_codelist
     )
 
-    return (age >= 12) | (pregnancy_status.is_not_null() & (age >= 16))
+    inclusion_criteria = age >= 12
+    exclusion_criteria = pregnancy_status & (age < 16)
+
+    return inclusion_criteria & ~exclusion_criteria
 
 
-def get_acute_otitis_media_denominator(index_date, selected_events, pregnancy_codelist):
+def get_acute_otitis_media_denominator(
+    index_date, patients, selected_events, pregnancy_codelist
+):
     acute_otitis_code = ["3110003"]
     count_acute_otitis_6m = count_past_events(
         index_date, selected_events, acute_otitis_code, 6
@@ -120,15 +126,19 @@ def get_acute_otitis_media_denominator(index_date, selected_events, pregnancy_co
         index_date, selected_events, pregnancy_codelist
     )
 
-    return (
-        (age >= 1) & (age <= 17)
-        | (pregnancy_status.is_not_null() & (age >= 16))
-        | (count_acute_otitis_6m < 3)
-        | (count_acute_otitis_12m < 4)
+    inclusion_criteria = (age >= 1) & (age <= 17)
+    exclusion_criteria = (
+        (count_acute_otitis_6m >= 3)
+        | (count_acute_otitis_12m >= 4)
+        | (pregnancy_status & (age < 16))
     )
 
+    return inclusion_criteria & ~exclusion_criteria
 
-def get_latest_ethnicity(index_date, clinical_events, ethnicity_codelist, ethnicity_from_sus):
+
+def get_latest_ethnicity(
+    index_date, clinical_events, ethnicity_codelist, ethnicity_from_sus
+):
     latest_ethnicity_from_codes_category_num = (
         clinical_events.where(clinical_events.snomedct_code.is_in(ethnicity_codelist))
         .where(clinical_events.date.is_on_or_before(index_date))
@@ -157,18 +167,22 @@ def get_latest_ethnicity(index_date, clinical_events, ethnicity_codelist, ethnic
         when(ethnicity_from_sus.code.is_in(["H", "J", "K", "L"])).then(
             "Asian or Asian British"
         ),
-        when(ethnicity_from_sus.code.is_in(["M", "N", "P"])).then("Black or Black British"),
+        when(ethnicity_from_sus.code.is_in(["M", "N", "P"])).then(
+            "Black or Black British"
+        ),
         when(ethnicity_from_sus.code.is_in(["R", "S"])).then(
             "Chinese or Other Ethnic Groups"
         ),
     )
 
     ethnicity_combined = case(
-        when(latest_ethnicity_from_codes.is_not_null()).then(latest_ethnicity_from_codes),
-        when(latest_ethnicity_from_codes.is_null() & ethnicity_from_sus.is_not_null()).then(
-            ethnicity_from_sus
+        when(latest_ethnicity_from_codes.is_not_null()).then(
+            latest_ethnicity_from_codes
         ),
+        when(
+            latest_ethnicity_from_codes.is_null() & ethnicity_from_sus.is_not_null()
+        ).then(ethnicity_from_sus),
         otherwise="Missing",
     )
 
-    return(ethnicity_combined)
+    return ethnicity_combined
---
+++ analysis/test_pf_dataset_definition.py
@@ -0,0 +1,89 @@
+from datetime import date
+from analysis.pf_dataset_definition import dataset
+
+# Run the following command in the terminal to test the dataset definition dataset_definition_med_status_data_development
+# opensafely exec ehrql:v1 assure analysis/test_pf_dataset_definition.py
+# Index date = "2024-10-10"
+test_data = {
+    # Expected in population (test for age exclusions)
+    # Female patient, 16 year old
+    1: {
+        "patients": {"date_of_birth": date(2008, 1, 1), "sex": "female"},
+        "clinical_events": [
+            {},
+        ],
+        "ethnicity_from_sus": {},
+        "practice_registrations": [{"start_date": date(2024, 3, 1)}],
+        "expected_in_population": True,
+        "expected_columns": {
+            "uti_denominator": True,
+            "shingles_denominator": False,
+            "impetigo_denominator": True,
+            "insectbite_denominator": True,
+            "sorethroat_denominator": True,
+            "sinusitis_denominator": True,
+            "otitismedia_denominator": True,
+            "pregnancy_status": False,
+        },
+    },
+    # Expected in population (test for pregnancy exclusions)
+    # Female patient, PREGNANT 15 year old
+    2: {
+        "patients": {"date_of_birth": date(2009, 1, 1), "sex": "female"},
+        "clinical_events": [
+            {
+                # Pregnancy code
+                "date": date(2024, 10, 3),
+                "snomedct_code": "77386006",
+            },
+        ],
+        "ethnicity_from_sus": {},
+        "practice_registrations": [{"start_date": date(2024, 3, 1)}],
+        "expected_in_population": True,
+        "expected_columns": {
+            "uti_denominator": False,
+            "shingles_denominator": False,
+            "impetigo_denominator": False,
+            "insectbite_denominator": False,
+            "sorethroat_denominator": False,
+            "sinusitis_denominator": False,
+            "otitismedia_denominator": False,
+            "pregnancy_status": True,
+        },
+    },
+    # Expected in population (test for multiple diagnoses exclusions)
+    # Female patient, 24 year old
+    3: {
+        "patients": {"date_of_birth": date(2000, 1, 1), "sex": "female"},
+        "clinical_events": [
+            {
+                # 1st UTI diagnosis
+                "date": date(2024, 10, 7),
+                "snomedct_code": "1090711000000102",
+            },
+            {
+                # 2nd UTI diagnosis
+                "date": date(2024, 10, 8),
+                "snomedct_code": "1090711000000102",
+            },
+            {
+                # 3rd UTI diagnosis
+                "date": date(2024, 10, 9),
+                "snomedct_code": "1090711000000102",
+            },
+        ],
+        "ethnicity_from_sus": {},
+        "practice_registrations": [{"start_date": date(2024, 3, 1)}],
+        "expected_in_population": True,
+        "expected_columns": {
+            "uti_denominator": False,
+            "shingles_denominator": True,
+            "impetigo_denominator": True,
+            "insectbite_denominator": True,
+            "sorethroat_denominator": True,
+            "sinusitis_denominator": True,
+            "otitismedia_denominator": False,
+            "pregnancy_status": False,
+        },
+    },
+}
