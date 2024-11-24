e18ce660b371cab2374057644882a246f28458a0
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-11-24 12:30:45 +0000

    Improve` get_dataset_table_schema()`

--- lib/functions/get_dataset_nhsbsa_table_schema.R
+++ lib/functions/get_dataset_nhsbsa_table_schema.R
@@ -8,13 +8,15 @@ library("lubridate")
 library("tidyverse")
 library("readr")
 
+source(here("lib/functions/get_pf_medication_validation_data.R"))
+
 # This may be useful for writing SQL queries but reading the schema from
 # JSON in the metadata seems more tricky and maybe there's another way
 get_dataset_table_schema <- function(dataset_id) {
   base_endpoint <- "https://opendata.nhsbsa.net/api/3/action/"
   package_show_method <- "package_show?id="
 
-  available_datasets <- get_available_datasets(remove_foi = FALSE)
+  available_datasets <- get_available_datasets()
 
   if (!dataset_id %in% available_datasets) {
     stop("The provided 'dataset_id' is not available. Run 'get_available_datasets()' to see all available datasets.", call. = FALSE)
@@ -22,7 +24,7 @@ get_dataset_table_schema <- function(dataset_id) {
 
   metadata_response <- GET(paste0(base_endpoint, package_show_method, dataset_id))
   resources <- content(metadata_response)$result$resources
-  
+
   schema_raw <- resources[[1]]$schema
 
   # There seems to be a lot of odd strings in the JSON
@@ -36,15 +38,17 @@ get_dataset_table_schema <- function(dataset_id) {
   schema_fixed <- gsub("']", '"]', schema_fixed)
   schema_fixed <- gsub("-", "", schema_fixed)
   schema_fixed <- gsub("-", "", schema_fixed)
-  tidyjson::json(schema_fixed)
+
   schema_list <- fromJSON(schema_fixed, flatten = TRUE)
 
   tibble(schema_list$fields) |>
     select(name, title, type, description)
 }
 
-get_available_datasets()[3:10]
-get_dataset_table_schema("contractor-details")
-get_dataset_table_schema("dental-activity-delivered-by-newly-qualified-foundation-dentists")
-get_dataset_table_schema("secondary-care-medicines-data")
-map(get_available_datasets()[3:10], get_dataset_table_schema)
+nhsbsa_table_schemas <- map(
+  set_names(get_available_datasets()),
+  safely(get_dataset_table_schema)
+)
+
+nhsbsa_table_schemas_results <- map(nhsbsa_table_schemas, "result")
+nhsbsa_table_schemas_errors <- map(nhsbsa_table_schemas, "error")
