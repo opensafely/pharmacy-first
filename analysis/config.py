6e83204de28ad59393b3bcd4b374aaff438a630e
viv3ckj <viveck.kingsley@gmail.com>
2025-01-09 12:23:47 +0000

    Update validation datasets and study datasets to incude latest month

--- analysis/config.py
+++ analysis/config.py
@@ -1,19 +1,19 @@
 # measures_definition_pf_breakdown.py
 start_date_measure_pf_breakdown = "2023-11-01"
-monthly_intervals_measure_pf_breakdown = 12
+monthly_intervals_measure_pf_breakdown = 13
 
 # measures_definition_pf_condition_provider.py
 start_date_measure_condition_provider = "2023-01-01"
-monthly_intervals_measure_condition_provider = 22
+monthly_intervals_measure_condition_provider = 23
 
 # measures_definition_pf_descriptive_stats.py
 start_date_measure_descriptive_stats = "2024-02-01"
-monthly_intervals_measure_descriptive_stats = 9
+monthly_intervals_measure_descriptive_stats = 10
 
 # measures_definition_pf_medications.py
 start_date_measure_medications = "2023-11-01"
-monthly_intervals_measure_medications = 9
+monthly_intervals_measure_medications = 10
 
 # measures_definition_pf_consultation_pf_counts.py
 start_date_measure_med_counts = "2024-02-01"
-monthly_intervals_measure_med_counts = 6
+monthly_intervals_measure_med_counts = 7
