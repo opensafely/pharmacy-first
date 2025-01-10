5cdfbd2e225d51426bff5c669c04107264d40586
viv3ckj <viveck.kingsley@gmail.com>
2025-01-10 10:45:13 +0000

    Update all end dates to end on 11-01

--- analysis/config.py
+++ analysis/config.py
@@ -1,19 +1,19 @@
-# measures_definition_pf_breakdown.py
+# measures_definition_pf_breakdown.py (01/11/2023 - 01/11/2024)
 start_date_measure_pf_breakdown = "2023-11-01"
-monthly_intervals_measure_pf_breakdown = 13
+monthly_intervals_measure_pf_breakdown = 12
 
-# measures_definition_pf_condition_provider.py
+# measures_definition_pf_condition_provider.py (01/01/2023 - 01/11/2024)
 start_date_measure_condition_provider = "2023-01-01"
-monthly_intervals_measure_condition_provider = 23
+monthly_intervals_measure_condition_provider = 22
 
-# measures_definition_pf_descriptive_stats.py
+# measures_definition_pf_descriptive_stats.py (01/02/2024 - 01/11/2024)
 start_date_measure_descriptive_stats = "2024-02-01"
-monthly_intervals_measure_descriptive_stats = 10
+monthly_intervals_measure_descriptive_stats = 9
 
-# measures_definition_pf_medications.py
+# measures_definition_pf_medications.py (01/11/2023 - 01/11/2024)
 start_date_measure_medications = "2023-11-01"
-monthly_intervals_measure_medications = 10
+monthly_intervals_measure_medications = 12
 
-# measures_definition_pf_consultation_pf_counts.py
+# measures_definition_pf_consultation_pf_counts.py (01/02/2024 - 01/11/2024)
 start_date_measure_med_counts = "2024-02-01"
-monthly_intervals_measure_med_counts = 7
+monthly_intervals_measure_med_counts = 9
