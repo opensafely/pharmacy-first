b90df27c3e4cd86d18604d9d617791d4c0b6c451
viv3ckj <viveck.kingsley@gmail.com>
2025-01-14 12:11:17 +0000

    extend study period by 2 months to include Nov and Dec

--- analysis/config.py
+++ analysis/config.py
@@ -1,19 +1,19 @@
 # measures_definition_pf_breakdown.py (01/11/2023 - 01/11/2024)
 start_date_measure_pf_breakdown = "2023-11-01"
-monthly_intervals_measure_pf_breakdown = 12
+monthly_intervals_measure_pf_breakdown = 14
 
 # measures_definition_pf_condition_provider.py (01/01/2023 - 01/11/2024)
 start_date_measure_condition_provider = "2023-01-01"
-monthly_intervals_measure_condition_provider = 22
+monthly_intervals_measure_condition_provider = 24
 
 # measures_definition_pf_descriptive_stats.py (01/02/2024 - 01/11/2024)
 start_date_measure_descriptive_stats = "2024-02-01"
-monthly_intervals_measure_descriptive_stats = 9
+monthly_intervals_measure_descriptive_stats = 11
 
 # measures_definition_pf_medications.py (01/11/2023 - 01/11/2024)
 start_date_measure_medications = "2023-11-01"
-monthly_intervals_measure_medications = 12
+monthly_intervals_measure_medications = 14
 
 # measures_definition_pf_consultation_pf_counts.py (01/02/2024 - 01/11/2024)
 start_date_measure_med_counts = "2024-02-01"
-monthly_intervals_measure_med_counts = 9
+monthly_intervals_measure_med_counts = 11
