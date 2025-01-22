c7ffe629bb84562c8a67c54b8ccd0ea1089e4cfe
viv3ckj <viveck.kingsley@gmail.com>
2025-01-22 08:41:20 +0000

    Add sex, age_band, imd, ethnicity and region to dataset definition, with functions.

--- analysis/pf_dataset_definition.py
+++ analysis/pf_dataset_definition.py
@@ -1,8 +1,10 @@
-from ehrql import create_dataset
+from ehrql import create_dataset, case, when
 from ehrql.tables.tpp import (
     patients,
     clinical_events,
     practice_registrations,
+    addresses,
+    ethnicity_from_sus,
 )
 from pf_variables_library import check_pregnancy_status
 from pf_dataset import (
@@ -13,6 +15,9 @@ from pf_dataset import (
     get_acute_sore_throat_denominator,
     get_acute_sinusitis_denominator,
     get_acute_otitis_media_denominator,
+    get_latest_ethnicity,
+    get_age_band,
+    get_imd
 )
 
 import codelists
@@ -22,35 +27,37 @@ dataset = create_dataset()
 
 registration = practice_registrations.for_patient_on(index_date)
 
-# Create new columns for each denominator rule
+# Columns for demographics table
+dataset.sex = patients.sex
+dataset.age_band = get_age_band(patients, index_date)
+dataset.region = registration.practice_nuts1_region_name
+dataset.imd = get_imd(addresses, index_date)
+dataset.ethnicity = get_latest_ethnicity(
+    index_date, clinical_events, codelists.ethnicity_group16_codelist, ethnicity_from_sus, grouping=16
+)
+
+# Create new columns for each denominator rule for clinical conditions table
 dataset.uti_denominator = get_uncomplicated_uti_denominator(
     index_date, patients, clinical_events, codelists.pregnancy_codelist
 )
-
 dataset.shingles_denominator = get_shingles_denominator(
     index_date, patients, clinical_events, codelists.pregnancy_codelist
 )
-
 dataset.impetigo_denominator = get_impetigo_denominator(
     index_date, patients, clinical_events, codelists.pregnancy_codelist
 )
-
 dataset.insectbite_denominator = get_infected_insect_bites_denominator(
     index_date, patients, clinical_events, codelists.pregnancy_codelist
 )
-
 dataset.sorethroat_denominator = get_acute_sore_throat_denominator(
     index_date, patients, clinical_events, codelists.pregnancy_codelist
 )
-
 dataset.sinusitis_denominator = get_acute_sinusitis_denominator(
     index_date, patients, clinical_events, codelists.pregnancy_codelist
 )
-
 dataset.otitismedia_denominator = get_acute_otitis_media_denominator(
     index_date, patients, clinical_events, codelists.pregnancy_codelist
 )
-
 dataset.pregnancy_status = check_pregnancy_status(
     index_date, clinical_events, codelists.pregnancy_codelist
 )
