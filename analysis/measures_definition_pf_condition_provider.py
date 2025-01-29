a9e9b5e06b46afbe98d2e0ce2b18e09a89d77c9f
Milan Wiedemann <milan.wiedemann@gmail.com>
2025-01-29 06:45:44 +0100

    Run `ruff format`

--- analysis/measures_definition_pf_condition_provider.py
+++ analysis/measures_definition_pf_condition_provider.py
@@ -9,7 +9,10 @@ from analysis.measures_definition_pf_breakdown import (
     imd_quintile,
 )
 from codelists import pf_consultation_events_dict
-from config import start_date_measure_condition_provider, monthly_intervals_measure_condition_provider
+from config import (
+    start_date_measure_condition_provider,
+    monthly_intervals_measure_condition_provider,
+)
 from pf_variables_library import select_events
 
 measures = create_measures()
@@ -20,10 +23,15 @@ monthly_intervals = monthly_intervals_measure_condition_provider
 
 registration = practice_registrations.for_patient_on(INTERVAL.end_date)
 
-selected_events = select_events(clinical_events, start_date=INTERVAL.start_date, end_date=INTERVAL.end_date)
+selected_events = select_events(
+    clinical_events, start_date=INTERVAL.start_date, end_date=INTERVAL.end_date
+)
 
 # Create variable which contains boolean values of whether pharmacy first event exists for patient
-has_pharmacy_first = select_events(selected_events, codelist=pf_consultation_events_dict["pf_consultation_services_combined"]).exists_for_patient()
+has_pharmacy_first = select_events(
+    selected_events,
+    codelist=pf_consultation_events_dict["pf_consultation_services_combined"],
+).exists_for_patient()
 
 for condition_name, condition_code in pharmacy_first_conditions_codes.items():
     condition_events = selected_events.where(
@@ -32,16 +40,15 @@ for condition_name, condition_code in pharmacy_first_conditions_codes.items():
 
     # Define the numerator as the count of events for the condition
     numerator = condition_events.count_for_patient()
-    denominator = registration.exists_for_patient() & patients.sex.is_in(["male", "female"])
+    denominator = registration.exists_for_patient() & patients.sex.is_in(
+        ["male", "female"]
+    )
 
     # Measures for overall clinical services graph
     measures.define_measure(
         name=f"count_{condition_name}_total",
         numerator=numerator,
         denominator=denominator,
-        group_by={
-            "pf_status": has_pharmacy_first,
-            "imd": imd_quintile
-        },
+        group_by={"pf_status": has_pharmacy_first, "imd": imd_quintile},
         intervals=months(monthly_intervals).starting_on(start_date),
     )
