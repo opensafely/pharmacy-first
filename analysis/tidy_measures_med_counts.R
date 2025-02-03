a8e48e8287fa9e0992668764dfb300ca12620e53
viv3ckj <viveck.kingsley@gmail.com>
2025-02-03 15:48:24 +0000

    Make names easier to follow

--- analysis/tidy_measures_med_counts.R
+++ analysis/tidy_measures_med_counts.R
@@ -4,21 +4,21 @@ library(readr)
 library(gt)
 library(purrr)
 
-df <- read_csv(here("output", "measures", "pf_medications_measures.csv"))
-df1 <- read_csv(here("output", "measures", "consultation_med_counts_measures.csv"))
+df_pf_medications_measures <- read_csv(here("output", "measures", "pf_medications_measures.csv"))
+df_consultation_med_counts_measures <- read_csv(here("output", "measures", "consultation_med_counts_measures.csv"))
 
-df_medications <- df %>%
+df_medications <- df_pf_medications_measures %>%
     filter(numerator != 0)
 
-df_consultation_med_counts <- df1 %>%
+df_consultation_med_counts <- df_consultation_med_counts_measures %>%
     filter(numerator != 0)
 
 readr::write_csv(
   df_medications,
-  here::here("output", "measures", "pf_medications_measures_updated.csv")
+  here::here("output", "measures", "pf_medications_measures_tidy.csv")
 )
 
 readr::write_csv(
   df_consultation_med_counts,
-  here::here("output", "measures", "consultation_med_counts_measures_updated.csv")
+  here::here("output", "measures", "consultation_med_counts_measures_tidy.csv")
 )
