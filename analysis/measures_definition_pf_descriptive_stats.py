c7c85f2d0d7f19b5cbc2e73bacc34387dc0a4c7e
viv3ckj <viveck.kingsley@gmail.com>
2025-02-21 14:00:50 +0000

    Add additional descriptive stats and removed same_date stats

--- analysis/measures_definition_pf_descriptive_stats.py
+++ analysis/measures_definition_pf_descriptive_stats.py
@@ -15,7 +15,7 @@ from config import (
 
 measures = create_measures()
 measures.configure_dummy_data(population_size=100)
-measures.configure_disclosure_control(enabled=True)
+measures.configure_disclosure_control(enabled=False)
 
 start_date = start_date_measure_descriptive_stats
 monthly_intervals = monthly_intervals_measure_descriptive_stats
@@ -42,39 +42,42 @@ pf_consultation_events = select_events(
     codelist=pf_consultation_events_dict["pf_consultation_services_combined"],
 )
 
-# Extract Pharmacy First consultation IDs and dates
+# Extract Pharmacy First consultation IDs
 pf_ids = pf_consultation_events.consultation_id
-pf_dates = pf_consultation_events.date
-
 has_pf_consultation = pf_consultation_events.exists_for_patient()
+# Counts number of Pharmacy First consultations
+pf_consultation_count = pf_consultation_events.count_for_patient()
 
 # Select Pharmacy First conditions by ID and date
 selected_pf_id_conditions = selected_events.where(
     selected_events.consultation_id.is_in(pf_ids)
 ).where(selected_events.snomedct_code.is_in(pf_conditions_codelist))
 
-selected_pf_date_conditions = (
-    selected_events.where(selected_events.consultation_id.is_not_in(pf_ids))
-    .where(selected_events.date.is_in(pf_dates))
-    .where(selected_events.snomedct_code.is_in(pf_conditions_codelist))
+non_pf_condition_events = selected_events.except_where(
+    selected_events.snomedct_code.is_in(
+        pf_conditions_codelist
+    )).except_where(selected_events.snomedct_code.is_in(pf_consultation_events_dict["pf_consultation_services_combined"])
 )
 
 has_pf_id_condition = selected_pf_id_conditions.exists_for_patient()
-has_pf_date_condition = selected_pf_date_conditions.exists_for_patient()
+# Counts occurences of all Pharmacy First conditions per month
+pf_condition_count = selected_pf_id_conditions.count_for_patient()
+# Counts occurences of all non-Pharmacy First conditions from PF consultations per month
+nonpf_condition_count = non_pf_condition_events.where(non_pf_condition_events.consultation_id.is_in(pf_ids)).count_for_patient()
 
 # Select Pharmacy First Medications by ID and date
 selected_pf_id_medications = selected_medications.where(
     selected_medications.consultation_id.is_in(pf_ids)
 ).where(selected_medications.dmd_code.is_in(pf_med_codelist))
 
-selected_pf_date_medications = (
-    selected_medications.where(selected_medications.consultation_id.is_not_in(pf_ids))
-    .where(selected_medications.date.is_in(pf_dates))
-    .where(selected_medications.dmd_code.is_in(pf_med_codelist))
-)
+selected_nonpf_id_medications = selected_medications.where(
+    selected_medications.consultation_id.is_in(pf_ids)
+).except_where(selected_medications.dmd_code.is_in(pf_med_codelist))
+
 
 has_pf_id_medication = selected_pf_id_medications.exists_for_patient()
-has_pf_date_medication = selected_pf_date_medications.exists_for_patient()
+pf_med_count = selected_pf_id_medications.count_for_patient()
+nonpf_med_count = selected_nonpf_id_medications.count_for_patient()
 
 # Define measures
 measures.define_defaults(
@@ -102,18 +105,27 @@ measures.define_measure(
     numerator=has_pf_id_medication & has_pf_id_condition,
 )
 
-# Measures linked by Pharmacy First consultation date
 measures.define_measure(
-    name="pfmed_on_pfdate",
-    numerator=has_pf_date_medication,
+    name="pfconsultations_month_count",
+    numerator=pf_consultation_count,
 )
 
 measures.define_measure(
-    name="pfcondition_on_pfdate",
-    numerator=has_pf_date_condition,
+    name="pfconditions_month_count",
+    numerator=pf_condition_count,
 )
 
 measures.define_measure(
-    name="pfmed_and_pfcondition_on_pfdate",
-    numerator=has_pf_date_medication & has_pf_date_condition,
+    name="non_pfconditions_month_count",
+    numerator=nonpf_condition_count,
+)
+
+measures.define_measure(
+    name="pfmed_month_count",
+    numerator=pf_med_count,
+)
+
+measures.define_measure(
+    name="non_pfmed_month_count",
+    numerator = nonpf_med_count,
 )
