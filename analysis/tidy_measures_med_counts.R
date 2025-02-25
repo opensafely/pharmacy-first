75f6f9a64df2b3975a2b399171599f8d4504a5f4
Milan Wiedemann <milan.wiedemann@gmail.com>
2025-02-25 11:44:53 +0000

    Update PF med count measures
    We removed one measures action that was a duplicate (with less information), but kept the name as it was more consistent with other file names in this project. We also commented out the condition provider action as we wont need it at the moment.

--- analysis/tidy_measures_med_counts.R
+++ analysis/tidy_measures_med_counts.R
@@ -9,23 +9,10 @@ df_pf_medications_measures <- read_csv(
   col_types = cols(dmd_code = col_character())
 )
 
-df_consultation_med_counts_measures <- read_csv(
-  here("output", "measures", "consultation_med_counts_measures.csv"),
-  col_types = cols(dmd_code = col_character())
-)
-
 df_medications <- df_pf_medications_measures %>%
-    filter(numerator != 0)
-
-df_consultation_med_counts <- df_consultation_med_counts_measures %>%
     filter(numerator != 0)
 
 readr::write_csv(
   df_medications,
   here::here("output", "measures", "pf_medications_measures_tidy.csv")
-)
-
-readr::write_csv(
-  df_consultation_med_counts,
-  here::here("output", "measures", "consultation_med_counts_measures_tidy.csv")
 )
