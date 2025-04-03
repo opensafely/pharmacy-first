53b438385d1920c8a0f94b039ea4026fdabe03d4
Milan Wiedemann <milan.wiedemann@gmail.com>
2025-04-03 13:19:53 +0100

    Update table 1
    This allows us to compare the Pharmacy First population to the entire TPP population (registered and sex male or female).

--- analysis/create_tables.R
+++ analysis/create_tables.R
@@ -5,10 +5,11 @@ library(purrr)
 library(scales)
 
 # Dataset definition file path output/population/pf_population.csv.gz
-df_pf_tables <- read_csv(here("output", "population", "pf_tables.csv.gz"))
+df_pf_tab <- read_csv(here("output", "population", "pf_tables.csv.gz"))
 
-df_demographics_table <- df_pf_tables %>%
+df_demographics_table <- df_pf_tab %>%
   select(
+    has_pf_consultation,
     sex,
     age_band,
     region,
@@ -16,8 +17,10 @@ df_demographics_table <- df_pf_tables %>%
     ethnicity
   )
 
-df_pf_pathways_table <- df_pf_tables %>%
+df_pf_pathways_tab <- df_pf_tab %>%
+  filter(has_pf_consultation == TRUE) %>%
   select(
+    -has_pf_consultation,
     sex,
     region,
     imd,
@@ -35,6 +38,7 @@ df_pf_pathways_breakdown_variables <- c(
   "region",
   "imd"
 )
+
 df_pf_pathways_num_den_variables <- c(
   "uti_numerator",
   "shingles_numerator",
@@ -45,42 +49,58 @@ df_pf_pathways_num_den_variables <- c(
   "otitismedia_numerator"
 )
 
-# Table 1: Demographics
+# Define function to calculate demographics table counts
 # Count subcategories (e.g. female/male) for each variable/categories (e.g. sex)
 # Var names, referred to as .x in code: sex, age_band, region, imd, ethnicity
-df_demographics_table_counts <- map_dfr(
-  names(df_demographics_table),
-  ~ df_demographics_table %>%
-    group_by(across(all_of(.x))) %>%
-    summarise(n = n()) %>%
-    mutate(category = .x) %>%
-    rename(subcategory = 1)
-) %>%
-  filter(n > 7) %>%
-  mutate(n = round(n / 5) * 5) %>%
-  select(category, subcategory, n) %>%
-  replace_na(list(subcategory = "Missing")) %>%
-  group_by(category) %>%
-  mutate(
-    pct = round(n / sum(n), 4),
-    table = "tab_demographics"
+get_demographics_table <- function(df) {
+  map_dfr(
+    names(df),
+    ~ df %>%
+      group_by(across(all_of(.x))) %>%
+      summarise(n = n()) %>%
+      mutate(category = .x) %>%
+      rename(subcategory = 1)
   ) %>%
-  ungroup() %>%
-  pivot_longer(
-    cols = c(n, pct),
-    names_to = "metric"
-  ) %>%
-  relocate(table)
+    filter(n > 7) %>%
+    mutate(n = round(n / 5) * 5) %>%
+    select(category, subcategory, n) %>%
+    replace_na(list(subcategory = "Missing")) %>%
+    group_by(category) %>%
+    mutate(
+      pct = round(n / sum(n), 4),
+      table = "tab_demographics"
+    ) %>%
+    ungroup() %>%
+    pivot_longer(
+      cols = c(n, pct),
+      names_to = "metric"
+    ) %>%
+    relocate(table)
+}
+
+# Table 1: Demographics table
+df_demo_tab_counts_pf <- df_demographics_table %>%
+  filter(has_pf_consultation == TRUE) %>%
+  select(-has_pf_consultation) %>%
+  get_demographics_table() %>%
+  mutate(population = "pharmacy_first") %>%
+  relocate(table, population)
+
+df_demo_tab_counts_tpp <- df_demographics_table %>%
+  select(-has_pf_consultation) %>%
+  get_demographics_table() %>%
+  mutate(population = "tpp") %>%
+  relocate(table, population)
 
 # Table 2: Clinical pathways
-df_pf_pathways_table_long <- df_pf_pathways_table %>%
+df_pf_pathways_tab_long <- df_pf_pathways_tab %>%
   pivot_longer(
     cols = all_of(df_pf_pathways_num_den_variables),
     names_to = "pf_pathway_count"
   ) %>%
   filter(value == TRUE)
 
-df_pf_pathways_table_counts_by_sex <- df_pf_pathways_table_long %>%
+df_pf_pathways_tab_counts_by_sex <- df_pf_pathways_tab_long %>%
   group_by(sex, pf_pathway_count) %>%
   count(value) %>%
   ungroup() %>%
@@ -105,9 +125,10 @@ df_pf_pathways_table_counts_by_sex <- df_pf_pathways_table_long %>%
     cols = numerator,
     names_to = "metric"
   ) %>%
-  relocate(table)
+  mutate(population = "pharmacy_first") %>%
+  relocate(table, population)
 
-df_pf_pathways_table_counts_by_imd <- df_pf_pathways_table_long %>%
+df_pf_pathways_tab_counts_by_imd <- df_pf_pathways_tab_long %>%
   group_by(imd, pf_pathway_count) %>%
   count(value) %>%
   ungroup() %>%
@@ -132,16 +153,18 @@ df_pf_pathways_table_counts_by_imd <- df_pf_pathways_table_long %>%
     cols = numerator,
     names_to = "metric"
   ) %>%
-  relocate(table)
+  mutate(population = "pharmacy_first") %>%
+  relocate(table, population)
 
 # Combine tables
-df_tables_combined <- bind_rows(
-  df_demographics_table_counts,
-  df_pf_pathways_table_counts_by_sex,
-  df_pf_pathways_table_counts_by_imd
+df_tabs_combined <- bind_rows(
+  df_demo_tab_counts_pf,
+  df_demo_tab_counts_tpp,
+  df_pf_pathways_tab_counts_by_sex,
+  df_pf_pathways_tab_counts_by_imd
 )
 
 readr::write_csv(
-  df_tables_combined,
+  df_tabs_combined,
   here::here("output", "population", "pf_tables.csv")
 )
