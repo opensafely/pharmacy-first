615e1095be39a51cc642e1bdf84d397e604ab806
Milan Wiedemann <milan.wiedemann@gmail.com>
2025-05-19 17:01:42 +0100

    Test all combinations to get ethnicity

--- analysis/test_dataset_definition_tables.py
+++ analysis/test_dataset_definition_tables.py
@@ -9,7 +9,7 @@ test_data = {
     # Female patient, 16 year old
     1: {
         "patients": {
-            "date_of_birth": date(2008, 1, 1),
+            "date_of_birth": date(2000, 1, 1),
             "sex": "female",
         },
         "clinical_events": [
@@ -22,7 +22,103 @@ test_data = {
         "expected_columns": {
             "has_pf_consultation": False,
             "sex": "female",
-            "age": 17,
+            "age": 25,
+        },
+    },
+    # Test ethnicity, Missing
+    2: {
+        "patients": {
+            "date_of_birth": date(2000, 1, 1),
+            "sex": "female",
+        },
+        "clinical_events": [
+            {},
+        ],
+        "addresses": {},
+        "ethnicity_from_sus": {},
+        "practice_registrations": [{"start_date": date(2024, 3, 1)}],
+        "expected_in_population": True,
+        "expected_columns": {
+            "has_pf_consultation": False,
+            "sex": "female",
+            "age": 25,
+            "ethnicity": "Missing",
+        },
+    },
+    # Test ethnicity, use clinical event (no SUS entry present)
+    3: {
+        "patients": {
+            "date_of_birth": date(2000, 1, 1),
+            "sex": "female",
+        },
+        "clinical_events": [
+            {
+                # Swiss
+                "date": date(2000, 1, 1),
+                "snomedct_code": "76574004",
+            },
+        ],
+        "addresses": {},
+        "ethnicity_from_sus": {},
+        "practice_registrations": [{"start_date": date(2024, 3, 1)}],
+        "expected_in_population": True,
+        "expected_columns": {
+            "has_pf_consultation": False,
+            "sex": "female",
+            "age": 25,
+            "ethnicity": "Other White",
+        },
+    },
+    # Test ethnicity, use SUS when no clinical event
+    4: {
+        "patients": {
+            "date_of_birth": date(2000, 1, 1),
+            "sex": "female",
+        },
+        "clinical_events": [
+            {
+                # Swiss
+                "date": date(2000, 1, 1),
+                "snomedct_code": "76574004",
+            },
+        ],
+        "addresses": {},
+        "ethnicity_from_sus": {
+            "code": "G"
+        },
+        "practice_registrations": [{"start_date": date(2024, 3, 1)}],
+        "expected_in_population": True,
+        "expected_columns": {
+            "has_pf_consultation": False,
+            "sex": "female",
+            "age": 25,
+            "ethnicity": "Other Mixed",
+        },
+    },
+    # Test ethnicity, prioritise clinical event over SUS
+    5: {
+        "patients": {
+            "date_of_birth": date(2000, 1, 1),
+            "sex": "female",
+        },
+        "clinical_events": [
+            {
+                # Indian
+                "date": date(2000, 1, 1),
+                "snomedct_code": "154225001"
+            },
+        ],
+        "addresses": {},
+        "ethnicity_from_sus": {
+            "code": "L"
+        },
+        "practice_registrations": [{"start_date": date(2024, 3, 1)}],
+        "expected_in_population": True,
+        "expected_columns": {
+            "has_pf_consultation": False,
+            "sex": "female",
+            "age": 25,
+            "ethnicity": "Indian",
         },
     },
 }
