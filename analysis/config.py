298819c04ecb15f84faaefd4234e648214162b15
viv3ckj <viveck.kingsley@gmail.com>
2025-10-22 14:21:59 +0100

    Increase monthy intervals to Sep 2025

--- analysis/config.py
+++ analysis/config.py
@@ -4,9 +4,9 @@ start_date_dataset_tables = "2024-01-31"
 index_date_dataset_tables = "2025-01-31"
 
 # Measure: measures_definition_pf_breakdown.py
-# Timeframe: 01/11/2023 - 31/01/2025
+# Timeframe: 01/11/2023 - 30/09/2025
 start_date_measure_pf_breakdown = "2023-11-01"
-monthly_intervals_measure_pf_breakdown = 15
+monthly_intervals_measure_pf_breakdown = 23
 
 # Measure: measures_definition_pf_condition_provider.py
 # Timeframe: 01/01/2023 - 31/01/2025
@@ -14,16 +14,11 @@ start_date_measure_condition_provider = "2023-01-01"
 monthly_intervals_measure_condition_provider = 25
 
 # Measure: measures_definition_pf_descriptive_stats.py
-# Timeframe: 01/11/2023 - 31/01/2025
+# Timeframe: 01/11/2023 - 30/09/2025
 start_date_measure_descriptive_stats = "2023-11-01"
-monthly_intervals_measure_descriptive_stats = 15
-
-# Measure: measures_definition_pf_medications.py
-# Timeframe: 01/11/2023 - 31/01/2025
-start_date_measure_medications = "2023-11-01"
-monthly_intervals_measure_medications = 15
+monthly_intervals_measure_descriptive_stats = 23
 
 # Measure: measures_definition_pf_consultation_pf_counts.py
-# Timeframe: 01/11/2023 - 31/01/2025
+# Timeframe: 01/11/2023 - 30/09/2025
 start_date_measure_med_counts = "2023-11-01"
-monthly_intervals_measure_med_counts = 15
+monthly_intervals_measure_med_counts = 23
