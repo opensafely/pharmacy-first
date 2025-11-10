ec485358d88924018eabc69387c490100b319503
viv3ckj <viveck.kingsley@gmail.com>
2025-11-10 10:46:27 +0000

    Add october data

--- analysis/config.py
+++ analysis/config.py
@@ -3,22 +3,22 @@
 start_date_dataset_tables = "2024-01-31"
 index_date_dataset_tables = "2025-01-31"
 
-# Measure: measures_definition_pf_breakdown.py
-# Timeframe: 01/11/2023 - 30/09/2025
+# Measure: measures_definition_pf_breakdown.py -- UPDATE FOR MONTHLY DASHBOARD
+# Timeframe: 01/11/2023 - 31/10/2025
 start_date_measure_pf_breakdown = "2023-11-01"
-monthly_intervals_measure_pf_breakdown = 23
+monthly_intervals_measure_pf_breakdown = 24
 
 # Measure: measures_definition_pf_condition_provider.py
 # Timeframe: 01/01/2023 - 31/01/2025
 start_date_measure_condition_provider = "2023-01-01"
 monthly_intervals_measure_condition_provider = 25
 
-# Measure: measures_definition_pf_descriptive_stats.py
-# Timeframe: 01/11/2023 - 30/09/2025
+# Measure: measures_definition_pf_descriptive_stats.py -- UPDATE FOR MONTHLY DASHBOARD
+# Timeframe: 01/11/2023 - 31/10/2025
 start_date_measure_descriptive_stats = "2023-11-01"
-monthly_intervals_measure_descriptive_stats = 23
+monthly_intervals_measure_descriptive_stats = 24
 
-# Measure: measures_definition_pf_consultation_pf_counts.py
-# Timeframe: 01/11/2023 - 30/09/2025
+# Measure: measures_definition_pf_consultation_pf_counts.py -- UPDATE FOR MONTHLY DASHBOARD
+# Timeframe: 01/11/2023 - 31/10/2025
 start_date_measure_med_counts = "2023-11-01"
-monthly_intervals_measure_med_counts = 23
+monthly_intervals_measure_med_counts = 24
